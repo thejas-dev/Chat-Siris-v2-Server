ab5d64c128065eb005ea3b7129a2dbc865d46a0b
thejas <thejaskala308@gmail.com>
2022-11-26 19:47:02 +0530

    make it better

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const server = app.listen(PORT,()=>{
 //
 const io= socket(server,{
 	cors:{
-		origin:"http://localhost:3000",
+		origin:"https://chat-siris-v2.vercel.app",
 		credentials:true,
 	},
 });
